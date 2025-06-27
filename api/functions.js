9da4ceacb6226a248f224a3fcb6fd02a5b30c591
Kang Min <kangzail7062@gmail.com>
2025-06-27 20:12:09 +0900

    Update functions.js

--- api/functions.js
+++ api/functions.js
@@ -1,11 +1,9 @@
 // api/functions.js
-const { primitiveFunctions } = require('./pipeline.js');
+const { primitiveFunctions } = require('./shared_functions.js'); // 공통 파일에서 함수 목록 가져오기
 
 module.exports = async (request, response) => {
-  // [추가된 기능] 어떤 HTTP 메서드로 요청이 오는지 로그를 남김
   console.log(`Incoming ${request.method} request to /api/functions`);
 
-  // GET과 POST 요청을 모두 허용하도록 수정
   if (request.method !== 'POST' && request.method !== 'GET') {
     return response.status(405).json({ error: 'Method Not Allowed' });
   }
